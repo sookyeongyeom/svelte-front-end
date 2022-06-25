631e809b09f85ea4136094fce9db1c36cd671e1c
sookyeongyeom <sookyeong.dev@gmail.com>
2022-06-26 05:18:58 +0900

    :books: p100

---
+++ chapter_05/01-context-api/button.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {getContext} from 'svelte';
+
+    let handleIncrementCount = getContext('incrementCount');
+    let count = getContext('count');
+
+    function myEvent() {
+			handleIncrementCount();
+			console.log(count);
+		}
+</script>
+
+<button on:click={myEvent}>count 초기값: [{count}]</button>
